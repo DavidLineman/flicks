6e4e8e413b90b0fc9ef73e90d9e2313421c37fea
DavidLineman <dline215@gmail.com>
2020-04-29 16:26:03 -0400

    catch up bro

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,9 +1,4 @@
 module ReviewsHelper
 
-  def average_stars(movie)
-    if movie.average_stars.zero?
-      content_tag(strong: "No Reviews!") 
-    else 
-      pluralize(number_with_precision(movie.average_stars), "star")
-  end
+
 end
