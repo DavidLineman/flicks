467587f7fcb0d3ef0dcfb562ab19e5787c347348
DavidLineman <dline215@gmail.com>
2020-05-05 15:19:32 -0400

    addded username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/  }, 
                     uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 10, allow_blank: true }
+  validates :password, length: { minimum: 6, allow_blank: true }
+
+  validates :username, format: { with: /\A[A-Z0-9]+\z/i }, 
+                       uniqueness: { case_sensitive: false }
 
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
